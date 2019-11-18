07697d5b627fd5a78837b26cc081f16abf57113b
Juraj Uhlar <juro.uhlar@gmail.com>
2019-11-18 16:35:29 +0000

    Invader movement improvements

--- scripts/invader.gd
+++ scripts/invader.gd
@@ -5,12 +5,20 @@ export (PackedScene) var explosion
 
 export (String) var shoot_key
 export (bool) var active = true
-export (NodePath) var backupInvader
+export (NodePath) var backupInvaderPath
+
 onready var bullet_container = get_node("bullet_container")
 onready var gun_timer = get_node("gun_timer")
+onready var tweenNode = get_node("Tween")
+
+var tweenValues
 
 func _ready():
 	add_to_group("invaders")
+	
+	tweenValues = [position, position + Vector2(60,0)]
+	start_tween()
+	
 
 func _process(delta):
 	if Input.is_action_pressed(shoot_key):
@@ -23,6 +31,10 @@ func shoot():
 		var b = bullet.instance()
 		bullet_container.add_child(b)
 		b.start_at(get_node("muzzle").global_position)
+		
+func start_tween():
+	tweenNode.interpolate_property(self, "position", tweenValues[0], tweenValues[1], 2.0, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0)
+	tweenNode.start()
 
 
 func _on_Invader_area_entered(area):
@@ -37,6 +49,12 @@ func die():
 	get_parent().add_child(expl)
 	expl.position = position
 	expl.play()
-	var backup = get_node(backupInvader)
-	backup.activate()
+	if (backupInvaderPath != null):
+		var backup = get_node(backupInvaderPath)
+		if (backup): 
+			backup.activate()
 	queue_free()
+
+func _on_Tween_tween_completed(object, key):
+	tweenValues.invert()
+	start_tween()
